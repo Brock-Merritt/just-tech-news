e30285eb63f8171f9a0ce985f9706f8105c7f010
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-11-16 12:26:44 -0500

    chapter 3 lesson 13

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
+
 class Post extends Model {}
+
+
+
 
 // create fields/columns for Post model
 Post.init(
@@ -38,3 +43,6 @@ Post.init(
       modelName: 'post'
     }
   );
+
+
+  module.exports = Post;
